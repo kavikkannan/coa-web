df6dd4513e7acd968b71ca54c80a056591078181
subi <rsubikksha@gmail.com>
2023-10-31 21:58:47 +0530

    31-10-11:30

--- components/bodyl.jsx
+++ components/bodyl.jsx
@@ -1,13 +1,18 @@
+'use client'
 import React from 'react'
-
+import { useRouter } from 'next/navigation'
 const bodyl = () => {
+  const router=useRouter();
+  const navi=async()=>{
+router.push('/add');
+  }
   return (
     <div className="body-container  min-h-screen">
       <section className="hero-section text-center py-10">
         <h1 className="text-4xl font-bold mb-4">Welcome to Binary Arithmetic</h1>
         <p className="text-lg  mb-4">Explore the fundamental operations of binary numbers in computer organization.</p>
         <img src="/binary_operations_image.jpg" alt="Binary Operations" className="mx-auto mb-8" />
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <button onClick={navi} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Get Started
         </button>
       </section>
